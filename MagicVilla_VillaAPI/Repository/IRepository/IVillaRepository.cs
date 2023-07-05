13f1beb5e869735cd4174655f3c861f336faf2ca
4 <94565000+Garra0@users.noreply.github.com>
2023-07-05 17:20:40 +0300

    S-6  repository
    making the interface repository and implementation it in the repository class

---
+++ MagicVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+using MagicVilla_VillaAPI.models;
+
+namespace MagicVilla_VillaAPI.Repository.IRepository
+{
+	public interface IVillaRepository
+	{
+        // return all villas
+        Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null);
+        // return one villa and use 'noTracking' by this command 'bool tracked=true' 
+        Task<Villa> Get(Expression<Func<Villa, bool>> filter = null , bool tracked=true);
+        Task Create(Villa entity);
+        Task Remove(Villa entity);
+        // to save the changes 
+        Task save();
+    }
+}
+
---
+++ MagicVilla_VillaAPI/Repository/VillaRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq.Expressions;
+using AutoMapper;
+using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.models;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    // class to implementation the IVillaRepository interface
+    public class VillaRepository : IVillaRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+        public VillaRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        // we implementation the seem functions from the Interface repository:
+
+        public async Task Create(Villa entity)
+        {
+            await _db.Villas.AddAsync(entity);
+            await save();
+        }
+
+        public async Task<Villa> Get(Expression<Func<Villa,bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<Villa> query = _db.Villas;
+            if(!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Villa>> GetAll(Expression<Func<Villa,bool>> filter = null)
+        {
+            IQueryable<Villa> query = _db.Villas;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task Remove(Villa entity)
+        {
+            _db.Villas.Remove(entity);
+            await save();
+        }
+
+        public async Task save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
+

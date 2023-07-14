88b2d65e196df0a54f416aa15c1323b96798bede
4 <94565000+Garra0@users.noreply.github.com>
2023-07-14 16:29:06 +0300

    S-8 create the login / Register / UniqueUser
    create userRipository and its functions
    
    the login function need to be have some Security then i made token and some things else...

---
+++ MagicVilla_VillaAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MagicVilla_VillaAPI.models;
+using MagicVilla_VillaAPI.models.Dto;
+
+namespace MagicVilla_VillaAPI.Repository.IRepository
+{
+	public interface IUserRepository
+	{
+		bool IsUniqueUser(string username);
+		Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+		Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+	}
+}
+
---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.models;
+using MagicVilla_VillaAPI.models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+	public class UserRepository : IUserRepository
+	{
+		ApplicationDbContext _db;
+        // we add this line for Login
+        private string secretKey;
+		public UserRepository(ApplicationDbContext db,IConfiguration configuration)
+		{
+			_db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+		}
+
+        public bool IsUniqueUser(string username)
+        {
+            // is user Unique?
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                // that mean its Unique
+                return true;
+            }
+            return false;
+            // other if state:
+            //return user == null;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower()
+            && u.Password == loginRequestDTO.Password);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            // if user was found generate JWT Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var Key = Encoding.ASCII.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.ID.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(Key),SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user
+            };
+            return loginResponseDTO;
+        }
+
+        public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            LocalUser user = new()
+            {
+                UserName = registerationRequestDTO.UserName,
+                Password = registerationRequestDTO.Password,
+                Name = registerationRequestDTO.Name,
+                Role = registerationRequestDTO.Role,
+            };
+
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+        }
+    }
+}
+

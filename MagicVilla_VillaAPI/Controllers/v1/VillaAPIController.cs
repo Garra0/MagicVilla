6f81147ad50c06ceca54eeed5a322b5eaf3bb721
4 <94565000+Garra0@users.noreply.github.com>
2023-07-20 13:48:39 +0300

    search by the name

--- MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
@@ -43,12 +43,13 @@ namespace MagicVilla_VillaAPI.Controllers
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         // store data every 30s --> the goal : when i try call getVillas its will loading , but next 30s wont load because the info will be in the cache
         [ResponseCache(CacheProfileName = "Default30")]                 // in the input will shown message (FilterOccupancy) because i write it in the name.
-        public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name ="FilterOccupancy")]int? occupancy)
+        public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name ="FilterOccupancy")]int? occupancy,
+            [FromQuery]string? search)
         {
             //[FromQuery(Name ="FilterOccupancy")]int? occupancy
             // FromQuery the inbut in the box
             // the box name "FilterOccupancy"
-            // int? --> mean im waiting int number
+            // int? --> mean im waiting int number (the qustion mark mean: maybe i will get null value)
             // the number input name 'occupancy'
             try
             {
@@ -62,6 +63,11 @@ namespace MagicVilla_VillaAPI.Controllers
                 else
                 {
                     villaList = await _dbVilla.GetAllAsync();
+                }
+                // search about villa have Occupancy=x and (Name="..." or Amenity="...").
+                if (!string.IsNullOrEmpty(search))
+                {
+                    villaList = villaList.Where(u => u.Name.ToLower().Contains(search));
                 }
  
                 _response.Result = _mapper.Map<List<VillaDTO>>(villaList);
